bc5a241d61539b9b5d1382075f984ab1bc82cc74
isackaik <97101511+isackaik@users.noreply.github.com>
2022-04-07 21:56:20 -0300

    primeiro commit

---
+++ adivinhação.py
@@ -0,0 +1,28 @@
+import random
+n = random.randint(1, 100)
+rodada = 1
+pontos = 1000
+tentativas_de_acerto = 0
+nivel = int(input("Escolha o nível do jogo: 1, 2 ou 3.\n"))
+if nivel == 1:
+    tentativas_de_acerto = 20
+elif nivel == 2:
+    tentativas_de_acerto = 10
+elif nivel == 3:
+    tentativas_de_acerto = 5
+for rodada in range(1, tentativas_de_acerto + 1):
+    chute = int(input('Rodada {} de {}. Digite um numero de 1 a 100: \n'.format(rodada, tentativas_de_acerto)))
+    acertou = chute == n
+    baixo = chute < n
+    alto = chute > n
+    if acertou:
+        print('Voce acertou')
+        break
+    elif baixo:
+        print('Tente mais alto')
+        pontos = pontos - (1000/tentativas_de_acerto)
+    elif alto:
+        print('Tente mais baixo')
+        pontos = pontos - (1000/tentativas_de_acerto)
+    print('Sua pontuação atual é: {}'.format(pontos))
+print('Fim de jogo. Sua pontuação final foi: ', pontos)
